3722485fb34ca8232c7b91a12bc84bbbabec0f79
Michael Terekhov <mykhailo.terekhov@nure.ua>
2021-02-18 23:44:08 +0200

    StartUpMenuFix(Added local registrtion)
    Just added local registration with creating new Local account(temporary)

--- RockPaperScissorsGame/RockPaperScissors/Models/Account.cs
+++ RockPaperScissorsGame/RockPaperScissors/Models/Account.cs
@@ -9,13 +9,38 @@ namespace RockPaperScissors.Models
         public Account()
         {
         }
+        public Account(string login,string email, string password, string firstName, string lastName )
+        {
+            Id = Guid.NewGuid();
+            Login = login;
+            Email = email;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
         [JsonPropertyName("id")]
         public Guid Id { get; private set; }
+        [JsonPropertyName("firstName")]
+        public string FirstName { get; private set; }
+        [JsonPropertyName("lastName")]
+        public string LastName { get; private set; }
         [JsonPropertyName("login")]
         public string Login { get; private set; }
         [JsonPropertyName("email")]
         public string Email { get; private set; }
         [JsonPropertyName("password")]
         public string Password { get; private set; }
+        
+        //Soon will be deleted(for testing mode!)
+        public override string ToString()
+        {
+            return $"Guid: {Id}\n" +
+                $"Firstname: {FirstName}\n" +
+                $"Lastname: {LastName}\n" +
+                $"Login: {Login}\n" +
+                $"Email: {Email}\n" +
+                $"Password: {Password}";
+        }
     }
 }
