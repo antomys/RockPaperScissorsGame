715888653cb36483b522d74b0a965f611431370b
Michael Terekhov <mykhailo.terekhov@nure.ua>
2021-02-23 13:22:47 +0200

    Test Abstract models for requestProcessorForClient

---
+++ RockPaperScissorsGame/Client/Services/RequestProcessor/RequestModels/IResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Client.Services.RequestModels
+{
+    internal interface IResponse
+    {
+        public bool Handled { get; }
+        public int Code { get; }
+        public string Content { get; }
+    }
+}
---
+++ RockPaperScissorsGame/Client/Services/RequestProcessor/RequestModels/RequestMethod.cs
@@ -0,0 +1,11 @@
+﻿namespace Client.Services.RequestModels
+{
+    internal enum RequestMethod
+    {
+        Undefined = 0,
+        Get = 1,
+        Post = 2,
+        Put = 3,
+        Patch = 4
+    }
+}
