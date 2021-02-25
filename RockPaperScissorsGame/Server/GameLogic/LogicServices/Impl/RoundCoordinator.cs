3e2f97b73a43dee2e85cdc67189065b903c0143e
IhorVol <ihor.volokhovych@unicyb.kiev.ua>
2021-02-25 17:19:23 +0200

    Added Statistics to RoundCoordinator.cs

--- RockPaperScissorsGame/Server/GameLogic/LogicServices/Impl/RoundCoordinator.cs
+++ RockPaperScissorsGame/Server/GameLogic/LogicServices/Impl/RoundCoordinator.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Server.GameLogic.Models;
 using Server.GameLogic.Models.Impl;
+using Server.Models;
 using Server.Services.Interfaces;
 
 namespace Server.GameLogic.LogicServices.Impl
@@ -12,16 +13,20 @@ namespace Server.GameLogic.LogicServices.Impl
     {
         private readonly IStorage<Round> _storageRounds;
 
+        private readonly IStorage<Statistics> _storageStatistics;
+
         private readonly IAccountManager _accountManager;
 
         public ConcurrentDictionary<string, Round> ActiveRounds { get; set; }
         
         public RoundCoordinator(
             IStorage<Round> storageRounds,
-            IAccountManager accountManager)
+            IAccountManager accountManager,
+            IStorage<Statistics> storageStatistics)
         {
             _storageRounds = storageRounds;
             _accountManager = accountManager;
+            _storageStatistics = storageStatistics;
             ActiveRounds = new ConcurrentDictionary<string, Round>();
         }
         
@@ -49,12 +54,15 @@ namespace Server.GameLogic.LogicServices.Impl
                         thisRound.IsDraw = true;
                         return null;
                     }
-                        
-
+                    
                     thisRound.IsFinished = true;
                     thisRound.WinnerId = winner;
                     thisRound.LoserId = thisRound.PlayerMoves.FirstOrDefault(x => x.Key != winner).Key;
                     thisRound.TimeFinished = DateTime.Now;
+
+                    await FillStatistics(thisRound,thisRound.WinnerId);
+                    await FillStatistics(thisRound,thisRound.LoserId);
+                    
                 }
                 
                 await UpdateRound(thisRound);
@@ -65,6 +73,66 @@ namespace Server.GameLogic.LogicServices.Impl
             return await await tasks;  //AWAIT AWAIT?
            
         }
+
+        private async Task FillStatistics(IRound thisRound, string accountId)
+        {
+            var statistics = await _storageStatistics.GetAsync(thisRound.LoserId);
+            if (thisRound.WinnerId.Equals(accountId))
+            {
+                statistics.Wins += 1;
+                statistics.Score += 4;
+            }
+            else
+            {
+                statistics.Loss += 1;
+                statistics.Score -= 2;
+            }
+
+            var playerMove =
+                thisRound.PlayerMoves.FirstOrDefault(x => x.Key.Equals(accountId)).Value;
+            switch (playerMove) //NOT TO ADD ANYTHING ELSE
+            {
+                case RequiredGameMove.Paper:
+                    statistics.UsedPaper += 1;
+                    break;
+                case RequiredGameMove.Rock:
+                    statistics.UsedRock += 1;
+                    break;
+                case RequiredGameMove.Scissors:
+                    statistics.UsedScissors += 1;
+                    break;
+            }
+
+            if (statistics.Loss != 0)
+                statistics.WinLossRatio = (double)statistics.Wins / statistics.Loss;
+
+            var allRound = await _storageRounds.GetAllAsync();
+            
+            int wins=0, loss=0;
+            
+            foreach (var round in allRound)
+            {
+                if (!InRange(round.TimeFinished,DateTime.Now.Date, DateTime.Now.Date.AddDays(-7))) continue;
+                if (round.WinnerId.Equals(accountId))
+                    wins++;
+                else
+                {
+                    loss++;
+                }
+            }
+
+            statistics.TimeSpent = loss != 0 
+                ? $"Last 7 days win rate: {(double) wins / loss}%" 
+                : $"Last 7 days win rate: {(double) wins}%";
+
+            await _storageStatistics.UpdateAsync(accountId, statistics);
+        }
+
+        private static bool InRange(DateTime dateToCheck, DateTime startDate, DateTime endDate)
+        {
+            return dateToCheck >= startDate && dateToCheck < endDate;
+        }
+        
         private async Task<Round> UpdateRound(Round updated)
         {
             var task = Task.Factory.StartNew(async () =>
@@ -127,4 +195,7 @@ namespace Server.GameLogic.LogicServices.Impl
         }
     }
 
+    internal class Datetime
+    {
+    }
 }
