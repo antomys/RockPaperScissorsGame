b3cc35e354c437fbd572618513a2970d3e885e5f
Ihor Volokhovych <igorvolokhovych@gmail.com>
2021-05-02 14:28:42 +0300

    Refactoring

---
+++ Server/Extensions/ServiceExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Server.GameLogic.LogicServices;
+using Server.GameLogic.LogicServices.Impl;
+using Server.Services;
+using Server.Services.Interfaces;
+
+namespace Server.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static IServiceCollection AddServices(this IServiceCollection service)
+        {
+           return service.AddSingleton(typeof(IDeserializedObject<>), typeof(DeserializedObject<>))
+            .AddTransient(typeof(IStorage<>), typeof(Storage<>))
+            .AddSingleton<IAccountManager, AccountManager>()
+            .AddSingleton<IRoundCoordinator, RoundCoordinator>()
+            .AddSingleton<IRoomCoordinator, RoomCoordinator>();
+        }
+    }
+}
--- Server/Startup.cs
+++ Server/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Server.Extensions;
 using Server.GameLogic.LogicServices;
 using Server.GameLogic.LogicServices.Impl;
 using Server.Services;
@@ -23,16 +24,9 @@ namespace Server
         private IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public static void ConfigureServices(IServiceCollection services)
         {
-            
-            services.AddSingleton(typeof(IDeserializedObject<>), typeof(DeserializedObject<>)); 
-            services.AddTransient(typeof(IStorage<>), typeof(Storage<>));
-
-            services.AddSingleton<IAccountManager, AccountManager>();
-            services.AddSingleton<IRoundCoordinator, RoundCoordinator>();
-            services.AddSingleton<IRoomCoordinator, RoomCoordinator>();
-            
+            services.AddServices();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -41,7 +35,7 @@ namespace Server
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -58,34 +52,7 @@ namespace Server
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.Map("/status/{sessionId}", async context =>
-                {
-                    var service = context.RequestServices.GetRequiredService<IAccountManager>();  //todo: remove
-                    
-                    var sessionId = (string) context.Request.RouteValues["sessionId"];
-
-                    if (sessionId == null)
-                    {
-                        context.Response.StatusCode = (int) HttpStatusCode.BadRequest;
-                    }
-                    else  if (await service.IsActive(sessionId))
-                    {
-                        context.Response.StatusCode = (int) HttpStatusCode.OK;
-                    }
-                    else
-                    {
-                        context.Response.StatusCode = (int) HttpStatusCode.Forbidden;
-                    }
-                });
-                
-                endpoints.Map("/status/", async context =>
-                {
-                    context.Response.StatusCode = (int) HttpStatusCode.OK;
-                    await context.Response.WriteAsync("alive");
-                });
-                
                 endpoints.MapControllers();
-                
             });
         }
     }
